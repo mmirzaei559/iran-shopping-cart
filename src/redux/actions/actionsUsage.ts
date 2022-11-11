c87e7899c05a0ac7d62c5e36d050b0010fcf7a89
Mohammad Mirzaei <mmirzaei559@gmail.com>
2022-11-12 00:22:08 +0330

    :art: Refactor: add actions usage

---
+++ src/redux/actions/actionsUsage.ts
@@ -0,0 +1,39 @@
+import { Dispatch } from 'redux';
+import { ActionType } from '../types';
+import { Action } from './actionsDefinition';
+
+export const selectProduct = (id: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.SELECT_PRODUCT,
+            payload: id,
+        });
+    };
+};
+
+export const increaseCartQuantity = (id: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.INCREASE_CART_QUANTITY,
+            payload: id,
+        });
+    };
+};
+
+export const decreaseCartQuantity = (id: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.DECREASE_CART_QUANTITY,
+            payload: id,
+        });
+    };
+};
+
+export const removeFromCart = (id: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.REMOVE_FROM_CART,
+            payload: id,
+        });
+    };
+};
